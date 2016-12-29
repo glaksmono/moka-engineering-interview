a21acb2dd31da635600bdf7aa051cb363b4118fd
Tito Pandu Brahmanto <tito@pandubrahmanto.com>
2016-12-29 20:32:11 +0700

    [MOKA-001] Finish the implementation of Create/Update/Delete for Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     # List all of the Items that are owned by the logged in User's Business
@@ -10,15 +11,44 @@ class ItemsController < ApplicationController
     end
   end
 
-  def create
-    # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+  def new
+    @item = Item.new
   end
 
-  def Update
-    # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item, notice: 'item was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+    @item.destroy
+    redirect_to items_url, notice: 'Item was successfully destroyed.'
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price)
   end
 end
--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @item = Item.create!(name: "Green Tea Latte", price: 35_000)
+    @item_hash = { name: "Double shot", price: 40_000 }
+    @user = User.create!(email: "test@test.com", password: "testpassword",
+                         first_name: "Test", last_name: "Test")
+    session[:user_id] = @user.id
+  end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+    assert_template :new
+  end
+
+  test 'should create item' do
+    assert_difference('Item.count') do
+      post :create, item: @item_hash
+    end
+
+    assert_redirected_to item_path(assigns(:item))
+  end
+
+  test 'should show item' do
+    get :show, id: @item
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get :edit, id: @item
+    assert_response :success
+  end
+
+  test 'should update item' do
+    patch :update, id: @item, item: @item_hash
+    assert_redirected_to item_path(assigns(:item))
+    assert_equal 'Double shot', assigns(:item).name
+    assert_equal 40_000, assigns(:item).price
+  end
+
+  test 'should destroy item' do
+    assert_difference('Item.count', -1) do
+      delete :destroy, id: @item
+    end
+
+    assert_redirected_to items_path
+  end
 end
