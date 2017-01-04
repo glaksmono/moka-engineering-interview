be41c1b86e7b2524d17f39cecb4689894a3e9c33
devifr <dfedogawa3@gmail.com>
2017-01-04 23:49:16 +0700

    Finish the implementation of Create/Update/Delete for Items

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,3 +1,21 @@
 class BusinessesController < ApplicationController
+  def new
+    @business = current_business
+  end
+ 
+  def update
+    @business = current_business
+    if @business.update business_params
+      flash[:success] = 'business successfully updated.'
+      redirect_to items_path
+    else
+      flash[:errors] = @business.errors.full_messages
+      render :edit
+    end
+  end
 
+  private
+  def business_params
+    params.require(:business).permit(:name, :address, :city)  
+  end
 end
