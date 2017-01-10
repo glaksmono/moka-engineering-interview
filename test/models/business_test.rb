ebef9b06fa067579475e40e7b8a0a6a820fb7709
a.nazmy@gmail.com <a.nazmy@gmail.com>
2017-01-10 09:27:24 +0700

    Added Business and Item Model Test

--- test/models/business_test.rb
+++ test/models/business_test.rb
@@ -4,4 +4,37 @@ class BusinessTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@business = Business.new(name: "Test business", address: "Business address Test", city: "Business City Test")
+  end
+
+  test "should be valid" do
+  	assert @business.valid?
+  end
+
+  test "name should be present" do
+  	@business.name = " "
+  	assert_not @business.valid?
+  end
+
+  test "address should be present" do
+  	@business.address = " "
+  	assert_not @business.valid?
+  end
+
+  test "city should be present" do
+  	@business.city = " "
+  	assert_not @business.valid?
+  end
+
+  test "name should not be too long" do
+  	@business.name = "a" * 151
+  	assert_not @business.valid?
+  end
+
+  test "city should not be too long" do
+  	@business.city = "a" * 151
+  	assert_not @business.valid?
+  end
 end
