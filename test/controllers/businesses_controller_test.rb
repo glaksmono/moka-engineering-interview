37c7687e44507f0c837fafcf0fb822c25ec0682c
a.nazmy@gmail.com <a.nazmy@gmail.com>
2017-01-10 10:18:57 +0700

    - Added Test Session Helper - Added Business Controller Test

--- test/controllers/businesses_controller_test.rb
+++ test/controllers/businesses_controller_test.rb
@@ -4,4 +4,55 @@ class BusinessesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+  	@user = User.create!(
+      first_name: "Test ",
+  		last_name:"User", 
+  		email: "test@example.com",
+  		password:"password",
+  		password_confirmation:"password")
+  
+  	@business = {
+  		name: "Business Test ",
+  		address:"Biz address test", 
+  		city: "Biz City Test"}
+  end
+
+  def create
+  	@business = Business.create!(name: "Business Test ",
+  		address:"Biz address test", 
+  		city: "Biz City Test", user: @user)
+  end
+
+  test 'should redirect to new' do
+  	login
+  	get :new
+  	assert_template :new
+  	assert_response :success
+  end
+
+  test 'should fail create business if name is empty' do
+    login
+    @business.delete(:name)
+    post :create, business: @business
+    assert_template :new
+    assert_response :success
+  end
+
+  test 'should fail create business if address is empty' do
+    login
+    @business.delete(:address)
+    post :create, business: @business
+    assert_template :new
+    assert_response :success
+  end
+
+  test 'should fail create business if city is empty' do
+  	login
+    @business.delete(:city)
+    post :create, business: @business
+    assert_template :new
+    assert_response :success
+  end
 end
