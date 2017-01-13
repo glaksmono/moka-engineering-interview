34065166e392871e30382e9f4da468268f0cb2d4
a.nazmy@gmail.com <a.nazmy@gmail.com>
2017-01-13 22:52:40 +0700

    Enhance Cancancan Ability and Authorisation in view and controller

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,35 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
+      
       user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      elsif user.seller? 
-        can :manage, @item
-      else
-        can :read, :all
+      if user.role.name == "Admin"
+        #Admin can only manage his own business, items and users
+         can :create, Business
+         can :update, Business do |business|
+            business.try(:user) == user
+         end
+         can :create, Item
+         can :manage, Item do |item|
+            item.try(:business).try(:user) == user
+         end
+         can :create, User
+         can :manage, User do |user|
+            user.try(:business).try(:user) == user
+         end
+      elsif user.role.name ==  "Seller"
+        #Seller can only manage his own business and items.
+         can :create, Business
+         can :create, Item
+         can :manage, Item do |item|
+            item.try(:business).try(:user) == user
+         end
+      elsif user.role.name ==  "Regular"
+        can :create, Business
+        #Reguler can only read his own business and items.
+        can :read, Item do |item|
+            item.try(:business).try(:user) == user
+         end
       end
     
     
