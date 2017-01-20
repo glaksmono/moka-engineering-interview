7945daa281e428d9f5008cbda9924587ebe0505e
Audy Vaksi <vaksipranata@gmail.com>
2017-01-20 20:07:53 +0700

    Add crud for item models (Create, Read, Update, Delete) [MOKA-001]

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,15 +10,45 @@ class ItemsController < ApplicationController
     end
   end
 
-  def create
-    # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+  def new
+    @item = Item.new
   end
 
-  def Update
+  def create
     # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path, notice: "Item #{@item.name} has ben Created"
+    else
+      render 'new'
+    end
+  end
+
+  def edit 
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+    @item = Item.find(params[:id])
+ 
+    if @item.update(item_params)
+      redirect_to edit_item_path(@item), notice: "Item has ben saved"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+    @item = Item.find(params[:id])
+    @item.destroy
+  
+    redirect_to items_path,notice: "Item has ben deleted"
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :price, :business_id)
+    end
 end
